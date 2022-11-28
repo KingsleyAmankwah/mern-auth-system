6ecfc49920e314369ade6278c2612b334c69224a
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-28 18:11:24 +0000

    user Routes setup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser, getMe } = require("../controllers/userControllers");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userControllers");
 const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
 module.exports = router;
