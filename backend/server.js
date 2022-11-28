a9d5047c249a9cfcc69be1fa912b2a496db02416
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-28 17:09:21 +0000

    json and urlencoded added

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(Port, () => console.log(`Server is runnig on Port ${Port}`));
