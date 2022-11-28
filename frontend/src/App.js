9b0d320d83afa65a0a95d4d53b781348248a5fc1
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-28 18:36:51 +0000

    App entry file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
