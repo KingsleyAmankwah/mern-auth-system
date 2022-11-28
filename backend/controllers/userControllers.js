96743851ed58318ccf6b75a78f11f9bf4c0d7d70
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-28 04:12:59 +0000

    Controllers setup

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,20 @@
+const User = require("../model/userModel");
+const asyncHanlder = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const registerUser = asyncHanlder(async (req, res) => {
+  res.status(200).json(`message: User registered`);
+});
+const loginUser = asyncHanlder(async (req, res) => {
+  res.status(200).json(`message: Authenticate user`);
+});
+const getMe = asyncHanlder(async (req, res) => {
+  res.status(200).json(`message: Authenticate user`);
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe
+};
