a93e0d63476c6c61b82f2bd8b086fb391a509602
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-29 05:25:33 +0000

    formData initialize

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
   return (
     <>
       <section class="h-full gradient-form md:h-screen background-radial-gradient">
