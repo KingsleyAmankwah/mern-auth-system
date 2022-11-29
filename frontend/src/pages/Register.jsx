c97c0ab7c5c75920bb752102fac89ea16188a274
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-29 05:55:25 +0000

    Register Page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { register } from "../features/auth/authSlice";
+import { toast } from "react-toastify";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -17,72 +21,93 @@ function Register() {
     }));
   };
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (e) => {
+    e.preventdefault();
+
+    const userData = {
+      name,
+      email,
+      password,
+    };
+
+    dispatch(register(userData))
+      .unwrap()
+      .then((user) => {
+        toast.success(`Welcome to mern auth system ${user.name}`);
+        navigate("/");
+      })
+      .catch(toast.error);
+  };
+
   return (
     <>
-      <section class="h-full gradient-form md:h-screen background-radial-gradient">
-        <div class="container py-12 px-6 h-full">
-          <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-            <div class="xl:w-10/12">
-              <div class="block bg-white shadow-lg rounded-lg">
-                <div class="lg:flex lg:flex-wrap g-0">
-                  <div class="lg:w-6/12 px-4 md:px-0">
-                    <div class="md:p-12 md:mx-6">
+      <section className="h-full gradient-form md:h-screen background-radial-gradient">
+        <div className="container py-12 px-6 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="xl:w-10/12">
+              <div className="block bg-white shadow-lg rounded-lg">
+                <div className="lg:flex lg:flex-wrap g-0">
+                  <div className="lg:w-6/12 px-4 md:px-0">
+                    <div className="md:p-12 md:mx-6">
                       <div className="text-center">
                         <h1 className="text-3xl font-semibold mt-1 mb-5 pb-1">
                           Register
                         </h1>
                       </div>
-                      <form>
+                      <form onSubmit={onSubmit}>
                         <p class="mb-4">Please Create an account...</p>
                         <div class="mb-4">
                           <input
                             type="text"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             placeholder="Full Name"
                             name="name"
                             value={name}
                             onChange={onChange}
-                            required
+                            // required
                           />
                         </div>
                         <div class="mb-4">
                           <input
                             type="email"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             placeholder="Email"
                             name="email"
                             value={email}
                             onChange={onChange}
-                            required
+                            // required
                           />
                         </div>
                         <div class="mb-4">
                           <input
                             type="password"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             placeholder="Password"
                             name="password"
                             value={password}
                             onChange={onChange}
-                            required
+                            // required
                           />
                         </div>
 
                         <div class="mb-4">
                           <input
                             type="password"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             placeholder="Confirm Password"
                             name="password2"
                             value={password2}
                             onChange={onChange}
-                            required
+                            // required
                           />
                         </div>
 
                         <div class="text-center pt-1 mb-12 pb-1">
                           <button
-                            class="inline-block px-6 py-2.5 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                            className="inline-block px-6 py-2.5 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
                             type="submit"
                           >
                             Register
