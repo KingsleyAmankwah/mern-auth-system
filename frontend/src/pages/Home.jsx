561065e3e29a8d34499e797e7a347046465f1712
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-29 05:55:08 +0000

    Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { FaSignOutAlt } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../features/auth/authSlice";
 
 function Home() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
   return (
-    <div>Home</div>
-  )
+    <div>
+      <header className="header">
+        <div className="logo">Mern Auth System</div>
+        <ul>
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        </ul>
+      </header>
+
+      <section className="heading">
+        <h1>Welcome to Mern Auth System</h1>
+        <p>Please choose from an option below</p>
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
