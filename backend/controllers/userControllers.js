38b93fe981ef5b942d6b7c90917f7514808ce2cd
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-09 21:03:25 +0000

    clean up

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,30 +3,23 @@ const asyncHanlder = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-// @desc    Register a new user
-// @route   /api/users/
-// @access  Public
 const registerUser = asyncHanlder(async (req, res) => {
   const { name, email, password } = req.body;
 
-  //validate user input
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please add all fields");
   }
 
-  //Check if user Exist
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error("User already exists");
   }
 
-  //Hash Password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  //Create new user
   const user = await User.create({
     name,
     email,
@@ -41,26 +34,18 @@ const registerUser = asyncHanlder(async (req, res) => {
       token: generateToken(user._id),
     });
   }
-
-  // res.status(200).json(`message: User registered`);
 });
 
-// @desc    Authenticate user
-// @route   /api/users/login
-// @access  Public
 const loginUser = asyncHanlder(async (req, res) => {
   const { email, password } = req.body;
 
-  //validate user input
   if (!email || !password) {
     res.status(400);
     throw new Error("Please add all fields");
   }
 
-  //Check if user exists
   const user = await User.findOne({ email });
 
-  //check user and password match
   if (user && (await bcrypt.compare(password, user.password))) {
     res.status(201).json({
       _id: user._id,
@@ -72,13 +57,8 @@ const loginUser = asyncHanlder(async (req, res) => {
     res.status(401);
     throw new Error("Invalid credentials");
   }
-
-  // res.status(200).json(`message: Authenticate user`);
 });
 
-// @desc    Get current user
-// @route   /api/users/me
-// @access  Private
 const getMe = asyncHanlder(async (req, res) => {
   res.status(200).json(req.user);
 });
