f064f3ed6f60f30a9f2cd095f0b80d79474698a3
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-09 21:26:26 +0000

    Code clean up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,43 +1,5 @@
-import React, { useEffect } from "react";
-import { FaSignOutAlt } from "react-icons/fa";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { logout } from "../features/auth/authSlice";
-
 function Home() {
-  const { user } = useSelector((state) => state.auth);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (!user) {
-      navigate("/login");
-    }
-  }, [user, navigate]);
-
-  const onLogout = () => {
-    dispatch(logout());
-    navigate("/login");
-  };
-  return (
-    <div className="container">
-      <header className="header">
-        <div className="logo">Mern Auth System</div>
-        <ul>
-          <li>
-            <button className="btn" onClick={onLogout}>
-              <FaSignOutAlt /> Logout
-            </button>
-          </li>
-        </ul>
-      </header>
-
-      <section className="heading">
-        <h1>Welcome to Mern Auth System</h1>
-        <p>{user && user.name}</p>
-      </section>
-    </div>
-  );
+  return <>Dashboard</>;
 }
 
 export default Home;
