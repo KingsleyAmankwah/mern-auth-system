7fadd4c029d614b22f60321e45b820b801ebb029
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-09 21:25:46 +0000

    Code clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-          </Routes>
-        </div>
-      </Router>
-      <ToastContainer />
+      <div className="App"></div>
     </>
   );
 }
