b28528bb6e666b9f71a07f16cacd33d2b3533773
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-09 21:10:07 +0000

    Code clean up

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
-const colors = require("colors");
-const { errorHandler } = require("./middlewares/errorMiddleware");
 
 const Port = process.env.PORT || 5000;
-connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use("/api/users", require("./routes/userRoutes"));
-
-app.use(errorHandler);
 
 app.listen(Port, () => console.log(`Server is runnig on Port ${Port}`));
