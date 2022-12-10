78ff7fdef40a516bd8b456b36a2d250a5974f436
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-10 03:20:20 +0000

    connectDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-
-const Port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+const colors = require("colors");
+const Port = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+connectDB();
 app.listen(Port, () => console.log(`Server is runnig on Port ${Port}`));
