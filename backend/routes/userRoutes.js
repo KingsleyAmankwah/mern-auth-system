1895cfd93bd37ea70f05889fb82addc1afdaa2b3
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-11 04:34:11 +0000

    protect middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {protect} = require('../middlewares/authMiddleware')
+const { protect } = require("../middlewares/authMiddleware");
 const {
   registerUser,
   sendVerificationEmail,
@@ -9,7 +9,7 @@ const {
 } = require("../controllers/userControllers");
 
 router.post("/register", registerUser);
-router.post("/sendVerificationEmail", sendVerificationEmail);
+router.post("/sendVerificationEmail", protect, sendVerificationEmail);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
 
