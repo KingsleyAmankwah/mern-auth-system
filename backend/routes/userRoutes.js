bbc58ac1bacf9a0c13f34e9cddd604bf9122e11a
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-11 04:07:04 +0000

    sendVerificationEmail Route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   registerUser,
+  sendVerificationEmail,
   loginUser,
   logoutUser,
 } = require("../controllers/userControllers");
 
 router.post("/register", registerUser);
+router.post("/sendVerificationEmail", sendVerificationEmail);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
 
