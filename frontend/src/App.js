ccbfa9e674e17a8efed53f7164e8d4b605e33a86
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-12 13:30:32 +0000

    App entry file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassowrd";
 
 function App() {
   return (
-    <>
-      <div className="App"></div>
-    </>
+    <div className="App bg-black m-0 p-0 w-full h-full">
+      <Router>
+        <Routes>
+          <Route path="/sign-in" element={<Login />} />
+          <Route path="/sign-up" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+
+      <ToastContainer />
+    </div>
   );
 }
 
