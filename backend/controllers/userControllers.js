fe1caad106b36993c0c27549d9871979e1962db6
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-12 04:07:39 +0000

    send Automated Email

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -285,6 +285,35 @@ const loginStatus = asyncHandler(async (req, res) => {
   }
 });
 
+//Send Automated emails
+const sendAutomatedEmail = asyncHandler(async (req, res) => {
+  const { subject, send_to, template, url } = req.body;
+
+  if (!subject || !send_to || !template) {
+    res.status(500);
+    throw new Error("Missing email parameter");
+  }
+
+  //Get user
+  const user = await User.findOne({ email: send_to });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  const sent_from = process.env.EMAIL_USER;
+  const name = user.name;
+  const link = `${process.env.FRONTEND_URL}${url}`;
+
+  try {
+    await sendEmail(subject, send_to, sent_from, template, name, link);
+    res.status(200).json({ message: "Email Sent" });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, try again");
+  }
+});
 //Send verification email
 const sendVerificationEmail = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
@@ -379,6 +408,7 @@ module.exports = {
   getUsers,
   upgradeUser,
   loginStatus,
+  sendAutomatedEmail,
   sendVerificationEmail,
   verifyUser,
 };
