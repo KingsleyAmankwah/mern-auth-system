99a66d6e5849da4e25a5b11d4fcc0a95ca6f467a
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-12 04:44:48 +0000

    reset password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -448,6 +448,33 @@ const forgotPassword = asyncHandler(async (req, res) => {
   }
 });
 
+//Reset Password
+const resetPassword = asyncHandler(async (req, res) => {
+  const { resetToken } = req.params;
+  const { password } = req.body;
+
+  const hashedToken = hashToken(resetToken);
+
+  const userToken = await Token.findOne({
+    rToken: hashedToken,
+    expiresAt: { $gt: Date.now() },
+  });
+
+  if (!userToken) {
+    res.status(404);
+    throw new Error("Invalid or Expired Token");
+  }
+
+  //Find User
+  const user = await User.findOne({ _id: userToken.userId });
+
+  // Now Reset password
+  user.password = password;
+  await user.save();
+
+  res.status(200).json({ message: "Password Reset Successful, please login" });
+});
+
 module.exports = {
   registerUser,
   loginUser,
@@ -462,4 +489,5 @@ module.exports = {
   sendVerificationEmail,
   verifyUser,
   forgotPassword,
+  resetPassword
 };
