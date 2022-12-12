b69de993bb064f09b1335e62e5e9843978926fd8
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-12 02:27:06 +0000

    verify User route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
 const {
   registerUser,
-  sendVerificationEmail,
   loginUser,
   logoutUser,
+  sendVerificationEmail,
+  verifyUser,
 } = require("../controllers/userControllers");
 
 router.post("/register", registerUser);
-router.post("/sendVerificationEmail", protect, sendVerificationEmail);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
+
+router.post("/sendVerificationEmail", protect, sendVerificationEmail);
+router.patch("/verifyUser/:verificationToken", verifyUser);
 
 module.exports = router;
