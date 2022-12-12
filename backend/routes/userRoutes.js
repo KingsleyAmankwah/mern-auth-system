f43b06121b5f41367e461c9b409107e69d611a80
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-12 04:07:13 +0000

    send Automated route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { protect , adminOnly, authorOnly } = require("../middlewares/authMiddleware");
+const {
+  protect,
+  adminOnly,
+  authorOnly,
+} = require("../middlewares/authMiddleware");
 const {
   registerUser,
   loginUser,
@@ -11,6 +15,7 @@ const {
   getUsers,
   upgradeUser,
   loginStatus,
+  sendAutomatedEmail,
   sendVerificationEmail,
   verifyUser,
 } = require("../controllers/userControllers");
@@ -21,11 +26,11 @@ router.get("/logout", logoutUser);
 router.get("/getUser", protect, getUser);
 router.patch("/updateUser", protect, updateUser);
 
-
 router.delete("/:id", protect, adminOnly, deleteUser);
 router.get("/getUsers", protect, authorOnly, getUsers);
 router.post("/upgradeUser", protect, adminOnly, upgradeUser);
 router.get("/loginStatus", loginStatus);
+router.post("/sendAutomatedEmail", protect, sendAutomatedEmail);
 
 router.post("/sendVerificationEmail", protect, sendVerificationEmail);
 router.patch("/verifyUser/:verificationToken", verifyUser);
