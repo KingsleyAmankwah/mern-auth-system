e90b26e79eb3cba68aab34c959e5d30453e693e8
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-14 02:13:58 +0000

    Register service

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -3,6 +3,15 @@ import axios from "axios";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API_URL = `${BACKEND_URL}/api/users/`;
 
-const authService = {};
+//Register user
+const register = async (userData) => {
+  const response = await axios.post(API_URL + "register", userData);
+
+  return response;
+};
+
+const authService = {
+  register,
+};
 
 export default authService;
