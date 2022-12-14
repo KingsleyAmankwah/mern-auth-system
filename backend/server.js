4b7f26d9565e7e523a9a7fd2e4fcaa843bdde015
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-14 04:08:20 +0000

    cors added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require("colors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/users", require("./routes/userRoutes"));
 
