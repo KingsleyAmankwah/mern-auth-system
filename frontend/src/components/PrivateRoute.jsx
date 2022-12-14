a7897f8bd19b1742bcbb1fd6d1f8b5f01a80373d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-14 16:39:15 +0000

    PrivateRoute

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useSelector((state) => state.auth);
+
+  return user ? children : <Navigate to="/sign-in" />;
+
+  // if (user) return <>{Children}</>;
+  // return <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
