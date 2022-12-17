3b9b29d2a1c595b98a7c6ef36b6e15a6d87f44f5
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-17 04:41:05 +0000

    Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HeaderStats from "../components/HeaderStats";
+import AdminNavbar from "../components/AdminNavbar";
+import Sidebar from "../components/Sidebar";
+import Profile from "./Profile";
+import Users from "./Users";
 
 function Home() {
-  return <>Dashboard</>;
+  return (
+    <>
+      <Sidebar />
+
+      <div className="relative md:ml-64 bg-blueGray-100 font-[system-ui]">
+        <AdminNavbar />
+        {/* Header */}
+        <HeaderStats />
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          <Routes>
+            <Route path="profile" element={<Profile />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="users" element={<Users />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
