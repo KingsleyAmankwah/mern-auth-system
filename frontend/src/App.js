adfd8388793e46e31adcb6ff5bbfec6907996ec0
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-17 04:39:22 +0000

    App entry file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,15 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassowrd";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App m-0 p-0 w-full h-full">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<Home />} />
           <Route path="/sign-in" element={<Login />} />
           <Route path="/sign-up" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
