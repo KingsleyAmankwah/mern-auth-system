c45eae846fab6b69fa76e9f9e042db989e7e1a75
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-18 06:51:58 +0000

    Landing Page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import NavBar from "../components/NavBar";
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
-  )
+    <>
+      <NavBar />
+      <main>
+        <section className="relative w-full h-full py-40 min-h-screen font-[system-ui]">
+          <div className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full bg-blueGray-800  bg-full bg-center bg-cover bg-[url('./images/register_bg_2.png')] "></div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
