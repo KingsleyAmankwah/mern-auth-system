d36fb24c054f77346bf08966a8e11bb6ef4529aa
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-20 03:27:03 +0000

    code clean up

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,8 +13,6 @@ const {
   updateUser,
   deleteUser,
   getUsers,
-  upgradeUser,
-  loginStatus,
   sendAutomatedEmail,
   sendVerificationEmail,
   verifyUser,
@@ -26,13 +24,11 @@ const {
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
-router.get("/getUser", protect, getUser);
-router.patch("/updateUser", protect, updateUser);
+router.get("/:id", protect, getUser);
+router.patch("/:id", protect, updateUser);
 
 router.delete("/:id", protect, adminOnly, deleteUser);
 router.get("/getUsers", protect, authorOnly, getUsers);
-router.post("/upgradeUser", protect, adminOnly, upgradeUser);
-router.get("/loginStatus", loginStatus);
 router.post("/sendAutomatedEmail", protect, sendAutomatedEmail);
 
 router.post("/sendVerificationEmail", protect, sendVerificationEmail);
