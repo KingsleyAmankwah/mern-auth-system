7022140820dcb86a4f5d8e0c3a833383bd6899b2
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-28 14:52:44 +0000

    app entry file

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,21 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
+dotenv.config();
+
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use("/api/users", require("./routes/userRoutes"));
+app.use(express.json());
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(errorHandler);
 
-const Port = process.env.PORT || 8000;
+const Port = process.env.PORT;
 
 app.listen(Port, () => {
   connectDB();
