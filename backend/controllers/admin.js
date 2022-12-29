04a10cd943c25e0ebc444e1ee0242bfd802140ae
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-29 13:53:33 +0000

    clean up

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,27 +1,6 @@
-const asynchandler = require("express-async-handler");
-const User = require("../models/userModel");
+// const asynchandler = require("express-async-handler");
+// const User = require("../models/userModel");
 
-const getUsers = asynchandler(async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(400);
-    throw new Error(error);
-  }
-});
 
-const deleteUser = asynchandler(async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("User has been deleted.");
-  } catch (error) {
-    res.status(400);
-    throw new Error(error);
-  }
-});
 
-module.exports = {
-  getUsers,
-  deleteUser,
-};
+
