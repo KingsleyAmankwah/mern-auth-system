d948763fc8c9326db9c35ed5f1a4a96e041dab54
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-29 04:40:57 +0000

    user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
     },
     photo: {
       type: String,
-      required: [true, "Please add a photo"],
+      // required: [true, "Please add a photo"],
       default: "https://i.ibb.co/4pDNDk1/avatar.png",
     },
     phone: {
