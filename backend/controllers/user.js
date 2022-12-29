14a53e63be36eafdaad04cbe274a2a28512bfd25
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-29 13:02:50 +0000

    user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const asynchandler = require("express-async-handler");
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 const getUser = asynchandler(async (req, res) => {
   try {
@@ -25,7 +26,40 @@ const updateUser = asynchandler(async (req, res) => {
   }
 });
 
+const changePassword = asynchandler(async (req, res) => {
+  const { id } = req.user;
+  const { password, newPassword } = req.body;
+
+  try {
+    const user = await User.findById(id);
+
+    if (user) {
+      const passwordIsCorrect = await bcrypt.compareSync(
+        password,
+        user.password
+      );
+
+      if (passwordIsCorrect) {
+        const salt = await bcrypt.genSaltSync(10);
+        const hashedPassword = await bcrypt.hashSync(newPassword, salt);
+
+        await User.findByIdAndUpdate(id, { password: hashedPassword });
+        res.status(200).json("Password changed successfully");
+      } else {
+        res.status(400);
+        throw new Error("Current Password is incorrect");
+      }
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   getUser,
   updateUser,
+  changePassword,
 };
