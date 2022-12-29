b358d5f979000e889689f40d5834b2f0684635f2
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-29 13:05:55 +0000

    user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -58,8 +58,30 @@ const changePassword = asynchandler(async (req, res) => {
   }
 });
 
+const getUsers = asynchandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error);
+  }
+});
+
+const deleteUser = asynchandler(async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
+  } catch (error) {
+    res.status(400);
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   getUser,
   updateUser,
   changePassword,
+  getUsers,
+  deleteUser,
 };
