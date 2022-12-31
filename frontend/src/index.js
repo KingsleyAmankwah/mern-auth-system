25af0c2c8f0ad028f380b480ed44f390fc8939de
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-31 18:10:32 +0000

    clean up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+/*eslint-disable*/
 
-const container = document.getElementById('root');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StateProvider } from "./context/StateProvider";
+import { AuthContextProvider } from "./context/AuthContext";
+import "./index.css";
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <StateProvider>
       <App />
-    </Provider>
+    </StateProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
