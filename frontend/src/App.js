2ccffdf6c26d50f7822032a2c5b8e84095cd7072
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-01 04:58:40 +0000

    Home Page Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,15 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassowrd";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App m-0 p-0 w-full h-full">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Login />} />
           <Route path="/sign-up" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
