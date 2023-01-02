a622b1915ba6f9b59e289d98874e1149941e6537
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-02 06:35:08 +0000

    user session data

--- frontend/src/context/StateProvider.js
+++ frontend/src/context/StateProvider.js
@@ -9,3 +9,24 @@ export const StateProvider = ({ reducer, initialState, children }) => (
 );
 
 export const useStateValue = () => useContext(StateContext);
+
+// export const AuthContextProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+//   useEffect(() => {
+//     localStorage.setItem("user", JSON.stringify(state.user));
+//   }, [state.user]);
+
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         user: state.user,
+//         loading: state.loading,
+//         error: state.error,
+//         dispatch,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
