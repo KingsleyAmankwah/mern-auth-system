be1963d1d3d587a9ca0d1e8e339a84d9b96b57a9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-02 06:34:11 +0000

    refresh token routes added

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { register, login, logout } = require("../controllers/auth");
+const {
+  register,
+  login,
+  logout,
+  getRefreshToken,
+} = require("../controllers/auth");
 
 const router = express.Router();
 
 router.post("/", register);
-router.post("/login", login);
 router.get("/", logout);
+router.post("/login", login);
+router.get("/refresh", getRefreshToken);
 
 module.exports = router;
