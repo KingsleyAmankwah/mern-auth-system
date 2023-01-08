0bdf9110df6f64cc2b9f2b8c56db807ef680f9af
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-08 21:16:57 +0000

    Redux added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,34 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import { StateProvider } from "./context/StateProvider";
+// import { initialState } from "./context/initialState";
+// import reducer from "./context/reducer";
+// import "./index.css";
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <StateProvider initialState={initialState} reducer={reducer}>
+//       <App />
+//     </StateProvider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { StateProvider } from "./context/StateProvider";
-import { initialState } from "./context/initialState";
-import reducer from "./context/reducer";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
+    <Provider store={store}>
       <App />
-    </StateProvider>
+    </Provider>
   </React.StrictMode>
 );
