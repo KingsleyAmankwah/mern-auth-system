7ad577357d0506a72445c73d7cb48ac8325bb4dc
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-08 21:23:20 +0000

    Protect

---
+++ frontend/src/components/Protect.jsx
@@ -0,0 +1,30 @@
+const { useSelector } = require("react-redux");
+const { selectIsLoggedIn, selectUser } = require("../features/auth/authSlice");
+
+export const ShowOnLogin = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (isLoggedIn) {
+    return <>{children}</>;
+  }
+  return null;
+};
+
+export const ShowOnLogout = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (!isLoggedIn) {
+    return <>{children}</>;
+  }
+  return null;
+};
+
+export const AdminAuthorLink = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const user = useSelector(selectUser);
+
+  if (isLoggedIn && (user?.role === "admin" || user?.role === "author")) {
+    return <>{children}</>;
+  }
+  return null;
+};
