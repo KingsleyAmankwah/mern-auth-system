a196fc66359ad72c6c17adc66de5c594e2198453
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-10 01:23:40 +0000

    Clean up

--- backend/controllers/admin.js
+++
@@ -1,6 +0,0 @@
-// const asynchandler = require("express-async-handler");
-// const User = require("../models/userModel");
-
-
-
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,8 @@
 /*eslint-disable*/
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassowrd";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -14,9 +10,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<Login />} />
-          <Route path="/sign-up" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </Router>
 
