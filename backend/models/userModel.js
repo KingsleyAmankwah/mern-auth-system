633f03662aa28ce7560d7e0975b9c7dc152d9ce0
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-10 01:51:14 +0000

    user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -11,6 +12,10 @@ const userSchema = mongoose.Schema(
       required: [true, "Please add an email"],
       unique: true,
       trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
     },
     password: {
       type: String,
@@ -18,29 +23,22 @@ const userSchema = mongoose.Schema(
     },
     photo: {
       type: String,
-      // required: [true, "Please add a photo"],
+      required: [true, "Please add a photo"],
       default: "https://i.ibb.co/4pDNDk1/avatar.png",
     },
     phone: {
       type: String,
-      default: "Phone Number",
+      default: "+234",
     },
     bio: {
       type: String,
-      default: "Tell us about yourself",
-    },
-    token: {
-      type: String,
-      default: "",
-    },
-    reset_token: {
-      type: String,
-      default: null,
+      default: "bio",
     },
     role: {
       type: String,
-      enum: ["subscriber", "admin"],
+      required: true,
       default: "subscriber",
+      // subscriber, author, and admin (suspended)
     },
     isVerified: {
       type: Boolean,
@@ -52,6 +50,19 @@ const userSchema = mongoose.Schema(
     minimize: false,
   }
 );
+
+// Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
