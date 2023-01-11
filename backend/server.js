3b313adcec8221780300132240e2c5e22cae534a
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-11 06:25:41 +0000

    server entry file

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,26 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: true }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use("/auth", require("./routes/authRoute"));
+app.use("/auth", require("./routes/userRoute"));
 
 app.use(errorHandler);
 
