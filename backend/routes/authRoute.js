d029f660f89bd1cb059593b2ffd1f4d5b415cf36
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-13 18:49:06 +0000

    auth routes

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -5,14 +5,12 @@ const {
   registerUser,
   loginUser,
   logoutUser,
-
   loginStatus,
 } = require("../controllers/auth");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
-
 router.get("/loginStatus", loginStatus);
 
 module.exports = router;
