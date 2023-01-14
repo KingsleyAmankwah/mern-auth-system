4b11ed8ad836f4423c371d2ee6ee945df193cc85
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-14 04:45:15 +0000

    user route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middlewares/authMiddleware");
-const { getUser } = require("../controllers/user");
+const {
+  protect,
+  adminOnly,
+  authorOnly,
+} = require("../middlewares/authMiddleware");
+const {
+  getUser,
+  updateUser,
+  deleteUser,
+  getUsers,
+} = require("../controllers/user");
 
 router.get("/getUser", protect, getUser);
+router.patch("/updateUser", protect, updateUser);
+router.delete("/:id", protect, adminOnly, deleteUser);
+router.get("/getUsers", protect, authorOnly, getUsers);
 
 module.exports = router;
