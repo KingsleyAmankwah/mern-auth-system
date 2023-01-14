b09d957f90807f4e64de038f301a4bbcc0d0ee8c
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-14 04:45:48 +0000

    user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -23,6 +23,65 @@ const getUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Get Users
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().sort("-createdAt").select("-password");
+  if (!users) {
+    res.status(500);
+    throw new Error("Something went wrong");
+  }
+  res.status(200).json(users);
+});
+
+// Update User
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { name, email, phone, bio, photo, role, isVerified } = user;
+
+    user.email = email;
+    user.name = req.body.name || name;
+    user.phone = req.body.phone || phone;
+    user.bio = req.body.bio || bio;
+    user.photo = req.body.photo || photo;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      phone: updatedUser.phone,
+      bio: updatedUser.bio,
+      photo: updatedUser.photo,
+      role: updatedUser.role,
+      isVerified: updatedUser.isVerified,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// Delete User
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = User.findById(req.params.id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  await user.remove();
+  res.status(200).json({
+    message: "User deleted successfully",
+  });
+});
+
 module.exports = {
   getUser,
+  getUsers,
+  updateUser,
+  deleteUser,
 };
