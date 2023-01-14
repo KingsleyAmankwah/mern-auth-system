66b24d20fa1b0044a8597f19103689f21fad49b4
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-14 23:15:40 +0000

    user slice

---
+++ frontend/src/features/user/userSlice.js
@@ -0,0 +1,113 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { extractErrorMessage } from "../../utils/utils";
+import { toast } from "react-toastify";
+import userService from "./userService";
+
+const initialState = {
+  user: null,
+  users: [],
+  isLoading: false,
+  message: "",
+};
+
+// Get User
+export const getUser = createAsyncThunk("user/getUser", async (_, thunkAPI) => {
+  try {
+    return await userService.getUser();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(extractErrorMessage(error));
+  }
+});
+
+// Update User
+export const updateUser = createAsyncThunk(
+  "user/updateUser",
+  async (userData, thunkAPI) => {
+    try {
+      return await userService.updateUser(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+
+// getUsers
+export const getUsers = createAsyncThunk(
+  "user/getUsers",
+  async (_, thunkAPI) => {
+    try {
+      return await userService.getUsers();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+// deleteUser
+export const deleteUser = createAsyncThunk(
+  "user/deleteUser",
+  async (id, thunkAPI) => {
+    try {
+      return await userService.deleteUser(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      // Get User
+      .addCase(getUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.message = action.payload;
+      })
+
+      // Update user
+      .addCase(updateUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        toast.success("User Updated");
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.message = action.payload;
+      })
+
+      // getUsers
+      .addCase(getUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        state.message = action.payload;
+      })
+
+      // deleteUser
+      .addCase(deleteUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        state.message = action.payload;
+      })
+      .addCase(deleteUser.rejected, (state, action) => {
+        state.message = action.payload;
+      });
+  },
+});
+
+export default userSlice.reducer;
