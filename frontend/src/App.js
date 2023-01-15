15c5776a2690336ebd875906de924e40cf5ac64b
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-15 08:09:26 +0000

    app entry file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,31 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import Layout from "./components/Layout";
+import Profile from "./pages/Profile";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+import {
+  getLoginStatus,
+  selectIsLoggedIn,
+  selectUser,
+} from "./features/auth/authSlice";
+import { getUser } from "./features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
 function App() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    dispatch(getLoginStatus());
+    if (isLoggedIn && user === null) {
+      dispatch(getUser());
+    }
+  }, [dispatch, isLoggedIn, user]);
+
   return (
     <div className="App m-0 p-0 w-full h-full">
       <Router>
@@ -15,6 +39,15 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/reset-password" element={<ForgotPassword />} />
+
+          <Route
+            path="/profile"
+            element={
+              <Layout>
+                <Profile />
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
 
