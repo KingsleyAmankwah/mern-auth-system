ca2452ae19612caead6eecdd44269f2cba4c6e06
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-16 03:05:50 +0000

    Sidenav component

--- frontend/src/components/Sidenav.jsx
+++ frontend/src/components/Sidenav.jsx
@@ -1,14 +1,14 @@
 /*eslint-disable*/
 import { FiHome, FiSettings, FiUsers } from "react-icons/fi";
-// import Link from "next/link";
 // import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-import { FaClipboardList } from "react-icons/fa";
-import { CgListTree } from "react-icons/cg";
-import { BsCalendarEvent } from "react-icons/bs";
+// import { FaClipboardList } from "react-icons/fa";
+// import { CgListTree } from "react-icons/cg";
+// import { BsCalendarEvent } from "react-icons/bs";
 import { BiLogOutCircle } from "react-icons/bi";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, RESET } from "../features/auth/authSlice";
 // import { AiOutlineAppstoreAdd } from "react-icons/ai";
 
 export const Navs = [
@@ -16,7 +16,7 @@ export const Navs = [
     name: "Dashboard",
     icon: <FiHome />,
     protected: false,
-    link: "",
+    link: "dashboard",
   },
 
   {
@@ -35,6 +35,15 @@ export const Navs = [
 function Sidenav({ page }) {
   const { user } = useSelector((state) => state.user);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutUser = async () => {
+    dispatch(RESET());
+    await dispatch(logout());
+    navigate("/login");
+  };
+
   return (
     <div className="bg-white px-4 h-[90vh] w-[20%] flex items-center justify-between">
       <div className="w-[85%] mx-auto h-full flex flex-col justify-between items-center pt-20">
@@ -45,7 +54,7 @@ function Sidenav({ page }) {
             if (nav.protected && user && user.role !== "admin") return;
 
             return (
-              <Link href={`/${nav.link}`} key={index}>
+              <a href={`${nav.link}`} key={index}>
                 <div
                   className={`flex gap-4 items-center cursor-pointer ${
                     page === nav.name
@@ -56,12 +65,12 @@ function Sidenav({ page }) {
                   {nav.icon}
                   <p className="">{nav.name}</p>
                 </div>
-              </Link>
+              </a>
             );
           })}
           <div
             className={`flex gap-4 items-center cursor-pointer text-active hover:bg-active-bg hover:text-active py-2 px-5 w-full rounded-full transition-all duration-75 ease-in-out`}
-            // onClick={() => LOGOUT(dispatch)}
+            onClick={logoutUser}
           >
             <BiLogOutCircle />
             <p>Logout</p>
