7e07dce74bd6cfc9b7792adbb204fded5580e713
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-16 03:33:44 +0000

    Navbar Component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,20 +17,7 @@ const Navbar = () => {
     <div className="bg-white py-2 px-4 w-screen h-[10vh] flex items-center z-40">
       {/* Brand */}
       <div className="flex justify-center items-center w-[20%] gap-5">
-        <Link href="/admin">
-          <div className="w-10 h-10 flex items-center justify-center border border-active-bg rounded-full">
-            <img
-              src="https://codersquiz.netlify.app/img/bentil.jpeg"
-              alt=""
-              className="w-[90%] h-[90%] border border-active-bg rounded-full"
-            />
-          </div>
-        </Link>
-        <Link href="/">
-          <h1 className="font-brand font-[500] text-3xl cursor-pointer">
-            Bentility
-          </h1>
-        </Link>
+        <p className="font-[500] text-sm cursor-pointer">Mern Auth System</p>
       </div>
       {/* main */}
       <div className="flex justify-end items-center w-[100%]">
