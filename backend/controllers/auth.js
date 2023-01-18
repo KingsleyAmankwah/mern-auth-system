b98acc8e38607603ed9ad3bfa3c98a1bb9a9fe28
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-18 05:48:27 +0000

    auth controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const Token = require("../models/tokenModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { generateToken } = require("../utils");
+const { generateToken, hashToken } = require("../utils");
 
-// Register User
+//! Register User
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -64,11 +65,11 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Login User
+//! Login User
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  //   Validation
+  // Validation
   if (!email || !password) {
     res.status(400);
     throw new Error("Please add email and password");
@@ -118,7 +119,7 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Logout User
+//! Logout User
 const logoutUser = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
     path: "/",
@@ -130,7 +131,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   return res.status(200).json({ message: "Logout successful" });
 });
 
-// Get Login Status
+//! Get Login Status
 const loginStatus = asyncHandler(async (req, res) => {
   const token = req.cookies.token;
   if (!token) {
@@ -146,9 +147,130 @@ const loginStatus = asyncHandler(async (req, res) => {
   return res.json(false);
 });
 
+//! Change Password
+const changePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, password } = req.body;
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error("Please enter old and new password");
+  }
+
+  //? Check if old password is correct
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  // Save new password
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+
+    res.status(200).json({ message: "Password change successful..." });
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect");
+  }
+});
+
+//! Forgot Password
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("No user with this email");
+  }
+
+  // Delete Token if it exists in DB
+  let token = await Token.findOne({ userId: user._id });
+  if (token) {
+    await token.deleteOne();
+  }
+
+  //   Create Verification Token and Save
+  const resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+  console.log(resetToken);
+
+  // Hash token and save
+  const hashedToken = hashToken(resetToken);
+  await new Token({
+    userId: user._id,
+    resetToken: hashedToken,
+    createdAt: Date.now(),
+    expiresAt: Date.now() + 60 * (60 * 1000), // 60mins
+  }).save();
+
+  // Construct Reset URL
+  const resetUrl = `${process.env.FRONTEND_URL}/resetPassword/${resetToken}`;
+
+  // Send Email
+  const subject = "Password Reset Request - AUTH:Z";
+  const send_to = user.email;
+  const sent_from = process.env.EMAIL_USER;
+  const reply_to = "noreply@king.com";
+  const template = "forgotPassword";
+  const name = user.name;
+  const link = resetUrl;
+
+  try {
+    await sendEmail(
+      subject,
+      send_to,
+      sent_from,
+      reply_to,
+      template,
+      name,
+      link
+    );
+    res.status(200).json({ message: "Password Reset Email Sent" });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again");
+  }
+});
+
+//! Reset Password
+const resetPassword = asyncHandler(async (req, res) => {
+  const { resetToken } = req.params;
+  const { password } = req.body;
+  console.log(resetToken);
+  console.log(password);
+
+  const hashedToken = hashToken(resetToken);
+
+  const userToken = await Token.findOne({
+    resetToken: hashedToken,
+    expiresAt: { $gt: Date.now() },
+  });
+
+  if (!userToken) {
+    res.status(404);
+    throw new Error("Invalid or Expired Token");
+  }
+
+  // Find User
+  const user = await User.findOne({ _id: userToken.userId });
+
+  // Now Reset password
+  user.password = password;
+  await user.save();
+
+  res.status(200).json({ message: "Password Reset Successful..." });
+});
+
 module.exports = {
   registerUser,
   loginUser,
   logoutUser,
   loginStatus,
+  changePassword,
+  forgotPassword,
+  resetPassword,
 };
