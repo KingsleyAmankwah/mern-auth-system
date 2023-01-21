c325752752c131618312eab1d2bfd1de6aac6ed7
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-21 05:15:23 +0000

    user route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -10,11 +10,24 @@ const {
   updateUser,
   deleteUser,
   getUsers,
+  sendAutomatedEmail,
+  sendVerificationEmail,
+  verifyUser,
+  forgotPassword,
+  resetPassword,
+  changePassword,
 } = require("../controllers/user");
 
 router.get("/getUser", protect, getUser);
 router.patch("/updateUser", protect, updateUser);
 router.delete("/:id", protect, adminOnly, deleteUser);
 router.get("/getUsers", protect, authorOnly, getUsers);
+router.post("/sendAutomatedEmail", protect, sendAutomatedEmail);
+
+router.post("/sendVerificationEmail", protect, sendVerificationEmail);
+router.patch("/verifyUser/:verificationToken", verifyUser);
+router.post("/forgotPassword", forgotPassword);
+router.patch("/resetPassword/:resetToken", resetPassword);
+router.patch("/changePassword", protect, changePassword);
 
 module.exports = router;
