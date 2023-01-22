f742878639c424e0e850b6ed44ef85bb0633594a
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-22 05:45:44 +0000

    send emails

--- backend/mail/sendEmail.js
+++ backend/mail/sendEmail.js
@@ -14,7 +14,7 @@ const sendEmail = async (
   // Create Email Transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 587,
+    port: 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -24,17 +24,19 @@ const sendEmail = async (
     },
   });
 
-  const handlearOptions = {
+  const viewsPath = path.join(__dirname, "../views");
+
+  const handlebarsOptions = {
     viewEngine: {
       extName: ".handlebars",
-      partialsDir: path.resolve("./views"),
+      partialsDir: viewsPath,
       defaultLayout: false,
     },
-    viewPath: path.resolve("./views"),
+    viewPath: viewsPath,
     extName: ".handlebars",
   };
 
-  transporter.use("compile", hbs(handlearOptions));
+  transporter.use("compile", hbs(handlebarsOptions));
 
   // Options f0r sending email
   const options = {
