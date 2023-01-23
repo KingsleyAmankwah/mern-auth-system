538c36a6de9ab61330910dbd7fb8da77620130ac
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-23 06:05:42 +0000

    user Service

--- frontend/src/features/user/userService.js
+++ frontend/src/features/user/userService.js
@@ -4,27 +4,66 @@ import axios from "axios";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API_URL = `${BACKEND_URL}/user/`;
 
-// Get user profile
+//! Get user profile
 const getUser = async () => {
   const response = await axios.get(API_URL + "getUser");
   return response.data;
 };
 
-// Update profile
+//! Update profile
 const updateUser = async (userData) => {
   const response = await axios.patch(API_URL + "updateUser", userData);
   return response.data;
 };
 
-// Get Users
+//! Get Users
 const getUsers = async () => {
   const response = await axios.get(API_URL + "getUsers");
 
   return response.data;
 };
-// Delete User
+//! Delete User
 const deleteUser = async (id) => {
   const response = await axios.delete(API_URL + id);
+
+  return response.data.message;
+};
+
+//! Change Password
+const changePassword = async (userData) => {
+  const response = await axios.patch(API_URL + "changePassword", userData);
+
+  return response.data.message;
+};
+
+//! forgot Password
+const forgotPassword = async (userData) => {
+  const response = await axios.post(API_URL + "forgotPassword", userData);
+
+  return response.data.message;
+};
+
+//! Reset Password
+const resetPassword = async (userData, resetToken) => {
+  const response = await axios.patch(
+    `${API_URL}resetPassword/${resetToken}`,
+    userData
+  );
+
+  return response.data.message;
+};
+
+//! Send Verification Email
+const sendVerificationEmail = async () => {
+  const response = await axios.post(API_URL + "sendVerificationEmail");
+  return response.data.message;
+};
+
+//! Verify User
+const verifyUser = async (verificationToken) => {
+  const response = await axios.patch(
+    `${API_URL}verifyUser/${verificationToken}`
+  );
 
   return response.data.message;
 };
@@ -34,6 +73,11 @@ const userService = {
   getUsers,
   updateUser,
   deleteUser,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+  sendVerificationEmail,
+  verifyUser,
 };
 
 export default userService;
