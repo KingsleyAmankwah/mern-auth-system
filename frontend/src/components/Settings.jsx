6a738f9c62f31b8e44199a3272c9c6f572a4c5cc
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-25 12:32:23 +0000

    change password

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { RESET } from "../features/auth/authSlice";
+import { changePassword } from "../features/user/userSlice";
+import useRedirectUser from "../hook/useRedirectUser";
+
 export default function Settings() {
+  useRedirectUser("/login");
+
+  const initialState = {
+    oldPassword: "",
+    password: "",
+    password2: "",
+  };
+  const [formData, setFormData] = useState(initialState);
+  const { oldPassword, password, password2 } = formData;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const updatePassword = async (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      return toast.error("Passwords do not match");
+    }
+
+    const userData = {
+      oldPassword,
+      password,
+    };
+
+    dispatch(changePassword(userData))
+      .then(() => dispatch(RESET(userData)))
+      .then(() => {
+        navigate("/login");
+      });
+  };
+
   return (
     <>
       <>
@@ -9,7 +54,7 @@ export default function Settings() {
             </div>
           </div>
           <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-            <form>
+            <form onSubmit={updatePassword}>
               <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
@@ -20,8 +65,11 @@ export default function Settings() {
                   </label>
                   <input
                     type="password"
-                    name="password"
+                    name="oldPassword"
+                    value={oldPassword}
+                    onChange={handleInputChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    required
                   />
                 </div>
               </div>
@@ -37,9 +85,10 @@ export default function Settings() {
                     <input
                       type="password"
                       name="password"
+                      value={password}
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      //   value={profile?.email}
-                      //   onChange={handleInputChange}
+                      onChange={handleInputChange}
+                      required
                     />
                   </div>
                 </div>
@@ -53,10 +102,11 @@ export default function Settings() {
                     </label>
                     <input
                       type="password"
-                      name="password"
+                      name="password2"
+                      value={password2}
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      //   value={profile?.email}
-                      //   onChange={handleInputChange}
+                      onChange={handleInputChange}
+                      required
                     />
                   </div>
                 </div>
@@ -68,7 +118,7 @@ export default function Settings() {
                     className="bg-green-500  hover:bg-green-700 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight rounded-xl shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
                     type="submit"
                   >
-                    Update Profile
+                    Change Password
                   </button>
                 </div>
               </div>
