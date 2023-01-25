969bbcd163f8b0de46f29edd3209769855dc1f18
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-25 04:09:03 +0000

    Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -70,20 +70,31 @@ const UserProfile = () => {
         />
         {isMenu && (
           <div className=" bg-white rounded-lg shadow-xl gap-0  flex-col absolute right-0 z-50 top-14 p-2 font-sans flex min-w-[12rem]">
-            <div
+            {/* <div
               className="text-active flex gap-2 items-center "
               onClick={showMenu}
             >
               <FaUserLock className="text-lg" />
-              <p>{user && user.name}</p>
-            </div>
+              <p>Profile</p>
+            </div> */}
+
             <div className="w-full h-[2px] my-2 bg-active-bg"></div>
-            <Link href="/profile" onClick={showMenu}>
+            <Link to="/profile">
               <div className="flex items-center gap-2 text-active cursor-pointer hover:bg-active-bg p-2">
-                <FiSettings className="text-lg" />
-                <p>settings</p>
+                <FaUserLock className="text-lg" />
+                <p>Profile</p>
               </div>
             </Link>
+
+            <div>
+              <Link to="/settings">
+                <div className="flex items-center gap-2 text-active cursor-pointer hover:bg-active-bg p-2">
+                  <FiSettings className="text-lg" />
+                  <p>Settings</p>
+                </div>
+              </Link>
+            </div>
+
             <div
               onClick={logoutUser}
               className="flex items-center gap-2 text-active cursor-pointer hover:bg-active-bg p-2"
