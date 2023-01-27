26f11bd2035c3b320fe7915910b433a110d560b5
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-27 19:02:49 +0000

    verify page

--- frontend/src/pages/Verify.js
+++ frontend/src/pages/Verify.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Loader from "../../components/loader/Loader";
-import { RESET, verifyUser } from "../../redux/features/auth/authSlice";
+import Spinner from "../components/Spinner";
+import { verifyUser } from "../features/user/userSlice";
+import { RESET } from "../features/auth/authSlice";
 
 const Verify = () => {
   const dispatch = useDispatch();
   const { verificationToken } = useParams();
 
-  const { isLoading } = useSelector((state) => state.auth);
+  const { isLoading } = useSelector((state) => state.user);
 
   const verifyAccount = async () => {
-    await dispatch(verifyUser(verificationToken));
-    await dispatch(RESET());
+    dispatch(verifyUser(verificationToken));
+    dispatch(RESET());
   };
 
   return (
     <section>
-      {isLoading && <Loader />}
-      <div className="--center-all">
+      {isLoading && <Spinner />}
+      <div>
         <h2>Account Verification</h2>
         <p>To verify your account, click the button below...</p>
         <br />
-        <button onClick={verifyAccount} className="--btn --btn-primary">
-          Verify Account
-        </button>
+        <button onClick={verifyAccount}>Verify Account</button>
       </div>
     </section>
   );
